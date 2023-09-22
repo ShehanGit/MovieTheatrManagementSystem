4a89a2a3921613503d9c5d6ce79bf9b48a1e3634
Shehan Vinod <110800392+ShehanGit@users.noreply.github.com>
2023-09-22 22:14:37 +0530

    ss
    ss

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,11 @@
 import React, {useState} from "react";
 import axios from "axios";
 import mainp from './Images/main.jpeg';
+import pic1 from './Images/p1.jpeg'
+import pic2 from './Images/p2.jpeg';
+import pic3 from './Images/p3.jpeg';
+import pic4 from './Images/p4.jpeg';
+import pic5 from './Images/p5.jpeg';
 
 
 export default function HomeMain(){
@@ -20,20 +25,25 @@ export default function HomeMain(){
 
         <div className="contn">
             
-            <div className="contn1">
-
+            <div className="contn1">          
+            <img className="ima1" src={pic1} alt="My Image" width="95%" height= "100%" />
+            <h3 className="h3">Oppenheimer</h3>
             </div>
             <div className="contn1">
-
+            <img className="ima1" src={pic5} alt="My Image" width="95%" height= "100%" />
+            <h3 className="h3">The Little Mermaid </h3> 
             </div>
             <div className="contn1">
-
+            <img className="ima1" src={pic3} alt="My Image" width="95%" height= "100%" />
+            <h3 className="h3">Spider-man </h3>
             </div>
             <div className="contn1">
-
+            <img className="ima1" src={pic4} alt="My Image" width="95%" height= "100%" />
+            <h3 className="h3">Guardian of the galaxy</h3>
             </div>
             <div className="contn1">
-
+            <img className="ima1" src={pic2} alt="My Image" width="95%" height= "100%" />
+            <h3 className="h3">Fast X</h3>
             </div>
 
         </div>
